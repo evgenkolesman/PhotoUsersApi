f882354a420059e31c1e9f29e59494a441d19a03
evgenkolesman <glavstroi_e@mail.ru>
2021-12-26 17:13:00 +0300

    commmit some changes

--- src/main/java/ru/koleson/photousersapi/controler/UserController.java
+++ src/main/java/ru/koleson/photousersapi/controler/UserController.java
@@ -1,2 +1,27 @@
-package ru.koleson.photousersapi.controler;public class UserController {
+package ru.koleson.photousersapi.controler;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+//    @GetMapping("/status/check")
+//    public Image status() throws IOException {
+//        return ImageIO.read(new URL("https://ya.cc/t/a6Ih_UVx37iLP5"));
+//    }
+
+    @GetMapping("/status/check")
+    public String status() {
+        return "Api is working";
+    }
 }
